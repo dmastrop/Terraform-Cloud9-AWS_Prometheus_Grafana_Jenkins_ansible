dd3af47f7fba8ada11f661b06030b680b6714788
Ubuntu <ubuntu@ip-172-31-26-107.us-west-1.compute.internal>
2023-07-27 18:24:36 +0000

    inital commit of providers.tf

---
+++ providers.tf
@@ -0,0 +1,15 @@
+# https://developer.hashicorp.com/terraform/language/providers/requirements
+# https://developer.hashicorp.com/terraform/language/providers/configuration
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+# my cloud9 is on us-west-1 as well (EC instance)
+provider "aws" {
+    region = "us-west-1"
+}
