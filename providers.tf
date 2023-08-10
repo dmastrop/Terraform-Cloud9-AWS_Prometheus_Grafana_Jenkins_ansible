92f5ea6c307b7bde2bcf538f8cbe7b4a6dbdb72b
Ubuntu <ubuntu@ip-172-31-26-107.us-west-1.compute.internal>
2023-08-10 20:11:48 +0000

    corrected incorrect syntax on shared_credentials_file. It shojld be shared_credentials_files=[/home/ubuntu/.aws/credentials]

--- providers.tf
+++ providers.tf
@@ -15,5 +15,9 @@ provider "aws" {
   # publish aws cloud9 ephemeral credentials to terraform and thus jenkins
   # jenkins will be able to apply resources to AWS
   # note that Jenkins is running locally on Cloud9 ubuntu instance and has access to this location
-  shared_credentials_file="/home/ubuntu/.aws/credentials"
+  #shared_credentials_file="/home/ubuntu/.aws/credentials"
+  # the shared_credentials_file is incorrect 
+  # it should be shared_credentials_files=["/home/ubuntu/.aws/credentials"]
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+  shared_credentials_files=["/home/ubuntu/.aws/credentials"]
 }
